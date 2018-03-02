14bbb78c3d992623af931e7d108526e145c04fe4
Chris Su <zerogpm@gmail.com>
2018-03-01 20:53:22 -0800

    create end-point for routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,33 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * Buyers
+ */
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+/**
+ * Categories
+ */
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+/**
+ * Product
+ */
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+/**
+ * Sellers
+ */
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+/**
+ *Transactions
+ */
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+
+/**
+ * Users
+ */
+Route::resource('users', 'User\UserController', ['only' => ['index', 'show']]);
