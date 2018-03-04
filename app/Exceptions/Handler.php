a77161319fb455824400fb1bbc81f49e1f15b58a
Chris Su <zerogpm@gmail.com>
2018-03-04 14:08:40 -0800

    handle unexpected exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,8 +7,10 @@ use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -79,6 +81,22 @@ class Handler extends ExceptionHandler
 
         if ($exception instanceof NotFoundHttpException) {
             return $this->errorResponse('The specified URL cannot be found' , 404);
+        }
+
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage() , $exception->getStatusCode());
+        }
+
+        if ($exception instanceof QueryException) {
+           $errorCode = $exception->errorInfo[1];
+
+           if ($errorCode === 1451) {
+               return $this->errorResponse('Cannot remove this resource permanently. It is related with any other resource', 409);
+           }
+        }
+
+        if (config('app.debug')) {
+            return $this->errorResponse('Unexpected Exception. Try later', 500);
         }
 
         return parent::render($request, $exception);
