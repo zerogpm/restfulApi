bf0c130b70af6c417e9828ff2244143f9f340ce6
Chris Su <zerogpm@gmail.com>
2018-03-15 23:12:55 -0700

    Add Index method for BuyerCategoryController

---
+++ app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use App\Http\Controllers\ApiController;
+
+class BuyerCategoryController extends ApiController
+{
+    public function index(Buyer $buyer)
+    {
+        $categories = $buyer->transactions()
+            ->with('product.categories')
+            ->get()
+            ->pluck('product.categories')
+            ->collapse()
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($categories);
+    }
+}
